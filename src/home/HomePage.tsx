971cd6e7e0a1d2ad5beff7f456d3319bbacd43ca
Tri Mao <trimao@TriMaos-Mac-mini.local>
2024-05-26 22:33:45 +0700

    test

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -6,6 +6,7 @@ function HomePage() {
 
     const [ldItems, setLdItems] = useState<LaunchDarkly>();
     const [loading, setLoading] = useState(Boolean);
+    const [filterkey, setFilterKey] = useState(String);
 
     useEffect(() => {
         async function loadProjects() {
@@ -24,25 +25,35 @@ function HomePage() {
         loadProjects();
     }, []);
 
+    const handleFilter = (event: any) => {
+        setFilterKey(event.target.value);
+    }
+
     return (
         <Fragment>
             <div>
-                {!loading && (<table>
-                    <tr>
-                        <th>Flag Name</th>
-                        <th>Status</th>
-                        <th>Description</th>
-                    </tr>
-                    {ldItems?.items?.filter((item) => item.tags.some((tag) => ['Saigon_Squad', 'Saigon'].includes(tag))).map((item) =>
-                        <tr>
-                            <td>{item.key}</td>
-                            <td>
-                                <input type="checkbox" name={item.key} id={item.key} checked={item.environments['sit-1'].on} onChange={() => {}} />
-                            </td>
-                            <td>{item.description}</td>
-                        </tr>
-                    )}
-                </table>)}
+                {!loading && (
+                    <div>
+                        <span>Filter: </span>
+                        <input name='filter' placeholder='input your flag name or key' onChange={handleFilter} />
+                        <table>
+                            <tr>
+                                <th>Flag Name</th>
+                                <th>Status</th>
+                                <th>Description</th>
+                            </tr>
+                            {ldItems?.items?.filter((item) => item.key.includes(filterkey)).map((item) =>
+                                <tr>
+                                    <td>{item.key}</td>
+                                    <td>
+                                        <input type="checkbox" name={item.key} id={item.key} checked={item.environments['sit-1'].on} onChange={() => { }} />
+                                    </td>
+                                    <td>{item.description}</td>
+                                </tr>
+                            )}
+                        </table>
+                    </div>)
+                }
             </div>
             {loading && (
                 <div className="top-center-page">
